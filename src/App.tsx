313561e996a79ddc63dc368bce2c20bc8dbdf0e7
maximobelets <maximobelets@gmail.com>
2023-01-25 12:12:23 +0500

    Add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Header } from './components/Header';
 import s from './App.module.scss';
 import { Projects } from './components/Projects';
 import { About } from './components/About';
+import { Footer } from './components/Footer/Footer';
 
 export const App = () => {
   return (
@@ -15,6 +16,7 @@ export const App = () => {
         {/* <Projects /> */}
         <About />
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import s from './Footer.module.scss';
+
+export const Footer = () => {
+    return (
+        <footer className={s.root}>
+            Footer
+        </footer>
+    )
+}

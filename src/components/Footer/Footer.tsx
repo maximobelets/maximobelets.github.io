1fb79aa99d44ca53938f983ba4084ec172e2ac25
maximobelets <maximobelets@gmail.com>
2023-11-15 17:06:28 +0500

    Add year to the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,14 @@
 import s from './Footer.module.scss';
 
 export const Footer = () => {
+    const currentYear = () => new Date().getFullYear();
+
     return (
         <footer className={s.root}>
             Footer
+            <p>
+                {currentYear()}
+            </p>
         </footer>
     )
 }
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,6 +3,19 @@ import { HashLink } from 'react-router-hash-link';
 import s from './Nav.module.scss';
 
 export const Nav = () => {
+    const hiThere  = () => {
+        const youCanReadThisText = true;
+
+        const simpleLogic = 10 > 1;
+
+        if (youCanReadThisText && simpleLogic) {
+            return 'Hi there 👋'
+        }
+    }
+
+    hiThere();
+
+
     return (
         <nav className={s.root}>
             <HashLink to="/#github" className={s.item}>

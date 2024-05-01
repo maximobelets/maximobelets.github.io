b2ce7fee380968fa3f316c144357451ead814fdb
maximobelets <maximobelets@gmail.com>
2024-05-01 22:47:08 +0500

    Add Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import { Nav } from "@/components/Nav";
-import Image from "next/image";
+
+import { Header } from "@/components/Header/Header";
+import { Nav } from "@/components/Nav/Nav";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-24">
       <Nav />
+      <Header />
     </main>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+import Photo from './assets/photo.png';
+
+export const Header = () => {
+    return (
+        <header className="mt-[240px] flex justify-between mx-[160px]">
+            <div>
+                <h2 className="text-[64px] font-bold">
+                    Hi there 👋
+                </h2>
+                <p className="text-5xl font-normal mt-[20px]">
+                    My name is Maxim Obelets<br />
+                    I'm a web developer
+                </p>
+            </div>
+            <Image 
+                src={Photo}
+                width={300}
+                height={300}
+                alt="Picture of the author"
+            />
+        </header>
+    )
+}

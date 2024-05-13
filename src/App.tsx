e6489789e5ff0709bfb68327f20e35e3c758f0f0
maximobelets <maximobelets@gmail.com>
2024-05-13 10:20:40 +0500

    Update styling issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Nav } from './components/Nav/Nav';
+import { GitHub } from './components/GitHub';
 
 import './App.css'
-import { Header } from './components/Header'
 
 function App() {
   return (
-    <Header />  
+    <BrowserRouter>
+
+      <div className='flex flex-col justify-between h-dvh px-[44px] py-[80px]'>
+
+        <Nav />
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Header />} />
+            <Route path="/github" element={<GitHub />} />
+          </Routes>
+        </main>
+
+        <Footer />
+
+      </div>
+
+    </BrowserRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.tsx'
-import './index.css'
-import { GitHub } from './components/GitHub/GitHub.tsx';
-import { Nav } from './components/Nav/Nav.tsx';
-import { Footer } from './components/Footer/Footer.tsx';
+import App from './App.tsx';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
     <React.StrictMode>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/github" element={<GitHub />} />
-        </Routes>
-        <Footer />
+      <App />
     </React.StrictMode>
-  </BrowserRouter>
 )

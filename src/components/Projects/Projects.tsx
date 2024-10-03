e7fe88d814cf9c614419088c17e435bbeac8f50b
maximobelets <maximobelets@gmail.com>
2024-10-03 11:16:03 +0500

    Update translations for projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,18 +1,21 @@
+import { useTranslation } from 'react-i18next';
 import { ProjectBlock } from '../ProjectBlock';
 
 import s from './Projects.module.css';
 
 export const Projects = () => {
+    const { t } = useTranslation();
+
     return (
         <section className={s.root}>
             <ProjectBlock 
                 title='react-headers'
-                description='React components library'
+                description={t("projects.reactHeaders")}
                 link='https://github.com/maximobelets/react-headers'
             />
             <ProjectBlock 
                 title='Frontend Interview'
-                description='Simple guide about Frontend interview'
+                description={t("projects.frontendInterview")}
                 link='https://github.com/maximobelets/Frontend-Interview'
             />
         </section>

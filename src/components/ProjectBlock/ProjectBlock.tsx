bb9c160cdbd172b742f6903154d9b4281fa8993a
maximobelets <maximobelets@gmail.com>
2024-11-11 18:26:25 +0500

    Fix error for deplot

--- src/components/ProjectBlock/ProjectBlock.tsx
+++ src/components/ProjectBlock/ProjectBlock.tsx
@@ -9,7 +9,7 @@ interface ProjectBlockProps {
     backgorund?: string;
 }
 
-export const ProjectBlock = ({ title, description, link = '', backgorund }: ProjectBlockProps) => {
+export const ProjectBlock = ({ title, description, link = '' }: ProjectBlockProps) => {
     return (
         <Link to={link} target='_blank' className={s.root}>
             <div className={s.info}>

8985442916af8e1466e4155385edad6edf5097c6
maximobelets <maximobelets@gmail.com>
2026-01-28 00:15:04 +0500

    Add Particle type

---
+++ src/components/Fireworks/types.ts
@@ -0,0 +1,19 @@
+export type Particle = {
+    // current x position
+    x: number;
+
+    // current y position
+    y: number;
+
+    // current x velocity
+    vx: number;
+
+    // current y velocity
+    vy: number;
+
+    // how many frames particle will be alive
+    life: number;
+
+    // color of particle
+    color: string;
+};
